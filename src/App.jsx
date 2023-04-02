285004074dfbd84858542e93cc7450722f72760c
c-kip <callumkipin@gmail.com>
2023-04-02 15:53:43 -0400

    Updated prettier formatting

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import PhrazzleField from "./components/Phrazzle-Field";
 
 function App() {
-  return (
-    <div className="Main">
-      <header className="Main-header">
-        <h2>Custom Phrazzle</h2>
-      </header>
-      <body>
-        <PhrazzleField />
-      </body>
-    </div>
-  );
+    return (
+        <div className="Main">
+            <header className="Main-header">
+                <h2>Custom Phrazzle</h2>
+            </header>
+            <body>
+                <PhrazzleField />
+            </body>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Phrazzle-Field.tsx
+++ src/components/Phrazzle-Field.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { TextField } from "@mui/material";
 
 function PhrazzleField() {
-  return <TextField label="Enter phrase here" variant="standard"></TextField>;
+    return <TextField label="Enter phrase here" variant="standard"></TextField>;
 }
 
 export default PhrazzleField;
