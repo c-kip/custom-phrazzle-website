9986770d50f0ed2927cf14ac812623f0b1de0f4b
c-kip <callumkipin@gmail.com>
2023-04-02 16:56:50 -0400

    Added theme to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
 import React from "react";
 import PhrazzleField from "./components/Phrazzle-Field";
 import Phrazzle from "./components/Phrazzle";
-import { createTheme } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material";
 
 // Create a color scheme
 const theme = createTheme({
-    letter: {
-        text: {
-            light: "#ffffff",
-            dark: "#000000",
-        },
-        background: {
-            guess: "#ffffff",
-            miss: "#757575",
-            diffWord: "#0d47a1",
-            close: "#ef6c00",
-            correct: "#00c853",
+    palette: {
+        letter: {
+            text: {
+                light: "#ffffff",
+                dark: "#000000",
+            },
+            background: {
+                guess: "#ffffff",
+                miss: "#757575",
+                diffWord: "#0d47a1",
+                close: "#ef6c00",
+                correct: "#00c853",
+            },
         },
     },
 });
 
 function App() {
     return (
-        <div>
+        <ThemeProvider theme={theme}>
             <header className="Main-header">
                 <h2>Custom Phrazzle</h2>
             </header>
@@ -30,7 +32,7 @@ function App() {
                 <PhrazzleField />
                 <Phrazzle phrase="apple for two" />
             </div>
-        </div>
+        </ThemeProvider>
     );
 }
 
