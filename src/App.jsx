72853b2b063f41e662d3ae8f2ccf80751c431a1a
c-kip <callumkipin@gmail.com>
2023-04-02 15:39:34 -0400

    Reformatted
    - Deleted several unnecessary files, moved some code, added new packages

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+    return (
+      <div className="Main">
+        <header className="Main-header">
+          <h2>Custom Phrazzle</h2>
+        </header>
+      </div>
+    );
+  }
+  
+  export default App;
+  
---
+++ src/components/Phrazzle-Field.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { TextField } from '@mui/material';
+
+function PhrazzleField() {
+  return (
+    <TextField label="Enter phrase here" variant='standard'></TextField>
+  );
+}
+
+export default PhrazzleField;
